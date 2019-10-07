cdd38ef8de3a0a5b51f1bd85804a1ade8e7adb63
Pablo Panero <ppanero@cern.ch>
2019-10-07 17:19:52 +0200

    global: fix copyright and prepare release

--- docs/conf.py
+++ docs/conf.py
@@ -317,8 +317,6 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/', None),
-    # TODO: Configure external documentation references, eg:
-    # 'Flask-Admin': ('https://flask-admin.readthedocs.io/en/latest/', None),
 }
 
 # Autodoc configuraton.
--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University,
+#                     Galter Health Sciences Library & Learning Center.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,9 +11,9 @@
 # Ignoring false positive 36759 (reporting invenio-admin v1.0.1). This can be
 # removed when https://github.com/pyupio/safety-db/pull/2274 is merged and
 # released.
-pipenv check --ignore 36759 && \
-pipenv run pydocstyle invenio_app_rdm tests docs && \
-pipenv run isort -rc -c -df && \
-pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
-pipenv run sphinx-build -qnNW docs docs/_build/html && \
-pipenv run test
+
+pydocstyle invenio_app_rdm tests docs && \
+isort -rc -c -df && \
+check-manifest --ignore ".travis-*,docs/_build*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test
