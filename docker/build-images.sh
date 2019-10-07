bd63c21af54c4c22842cd3edffed27cbdcb7d82c
Pablo Panero <ppanero@cern.ch>
2019-10-07 17:19:52 +0200

    global: fix copyright and prepare release

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University, Galter Health Sciences Library & Learning Center.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
