ea6cd9c3a2d74622ec245f6bce94c27ab0a97227
Guillaume Viger <fenekku@fenekku.com>
2019-11-26 11:55:54 -0600

    config: organize config [more]
    - Grouped configuration
    - Added documentation links
    - Kept ad-hoc order of groups but sorted alphabetically
      within a group
    - Removed some configurations that repeated defaults
      already provided by modules
    - Closes: #27

--- invenio_app_rdm/config.py
+++ invenio_app_rdm/config.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University,
-#                    Galter Health Sciences Library & Learning Center.
+# Copyright (C) 2019 Northwestern University.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Default configuration for Invenio App RDM.
 
-You overwrite and set instance-specific configuration by either:
+Each setting has a sensible default value. You overwrite and set
+instance-specific configuration by either:
 
 - Configuration file: ``<virtualenv prefix>/var/instance/invenio.cfg``
 - Environment variables: ``APP_<variable name>``
-"""
 
-from __future__ import absolute_import, print_function
+"""
 
 from datetime import timedelta
 
@@ -25,82 +24,144 @@ def _(x):
     return x
 
 
-# Rate limiting
+# Flask
+# =====
+# See https://flask.palletsprojects.com/en/1.1.x/config/
+
+APP_ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+"""Allowed hosts.
+
+Since HAProxy and Nginx route all requests no matter the host header
+provided, the allowed hosts variable is set to localhost. In production it
+should be set to the correct host and it is strongly recommended to only
+route correct hosts to the application.
+"""
+
+MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MiB
+"""Max upload size for form data via application/multipart-formdata."""
+
+SECRET_KEY = "CHANGE_ME"
+"""Flask secret key.
+
+Each installation (dev, production, ...) needs a separate key.
+
+SECURITY WARNING: keep the secret key used in production secret!
+"""
+
+SESSION_COOKIE_SECURE = True
+"""Sets cookie with the secure flag by default."""
+
+
+# Flask-Limiter
 # =============
-#: Storage for ratelimiter.
-RATELIMIT_STORAGE_URL = 'redis://localhost:6379/3'
+# https://flask-limiter.readthedocs.io/en/stable/#configuration
 
-# I18N
-# ====
-#: Default language
-BABEL_DEFAULT_LANGUAGE = 'en'
-#: Default time zone
+RATELIMIT_STORAGE_URL = "redis://localhost:6379/3"
+"""Storage for ratelimiter."""
+
+
+# Flask-Babel
+# ===========
+# See https://pythonhosted.org/Flask-Babel/#configuration
+
+BABEL_DEFAULT_LOCALE = 'en'
+"""Default locale (language)."""
+
 BABEL_DEFAULT_TIMEZONE = 'Europe/Zurich'
-#: Other supported languages (do not include the default language in list).
-I18N_LANGUAGES = [
-    # ('fr', _('French'))
-]
+"""Default time zone."""
 
-# Base templates
-# ==============
-#: Global base template.
+
+# Invenio-I18N
+# ============
+# See https://invenio-i18n.readthedocs.io/en/latest/configuration.html
+
+# Other supported languages (do not include BABEL_DEFAULT_LOCALE in list).
+# I18N_LANGUAGES = [
+#     ('fr', _('French'))
+# ]
+
+
+# Invenio-Theme
+# =============
+# See https://invenio-theme.readthedocs.io/en/latest/configuration.html
+
 BASE_TEMPLATE = 'invenio_app_rdm/page.html'
-#: Cover page base template (used for e.g. login/sign-up).
+"""Global base template."""
+
 COVER_TEMPLATE = 'invenio_theme/page_cover.html'
-#: Footer base template.
+"""Cover page base template (used for e.g. login/sign-up)."""
+
 FOOTER_TEMPLATE = 'invenio_theme/footer.html'
-#: Header base template.
+"""Footer base template."""
+
 HEADER_TEMPLATE = 'invenio_theme/header.html'
-#: Settings base template.
+"""Header base template."""
+
 SETTINGS_TEMPLATE = 'invenio_theme/page_settings.html'
+"""Settings base template."""
 
-# Theme configuration
-# ===================
-#: Site name
 THEME_SITENAME = _('Invenio App RDM')
-#: Use default frontpage.
+"""Site name."""
+
 THEME_FRONTPAGE = True
-#: Frontpage title.
-THEME_FRONTPAGE_TITLE = _('Invenio App RDM')
-#: Frontpage template.
+"""Use default frontpage."""
+
 THEME_FRONTPAGE_TEMPLATE = 'invenio_app_rdm/frontpage.html'
+"""Frontpage template."""
 
-# Email configuration
-# ===================
-#: Email address for support.
-SUPPORT_EMAIL = "info@inveniosoftware.org"
-#: Disable email sending by default.
+THEME_FRONTPAGE_TITLE = _('Invenio App RDM')
+"""Frontpage title."""
+
+
+# Invenio-Mail / Flask-Mail
+# =========================
+# See https://pythonhosted.org/Flask-Mail/#configuring-flask-mail
+
 MAIL_SUPPRESS_SEND = True
+"""Disable email sending by default."""
 
-# Assets
-# ======
-#: Static files collection method (defaults to copying files).
+
+# Flask-Collect
+# =============
+# See https://github.com/klen/Flask-Collect#setup
+
 COLLECT_STORAGE = 'flask_collect.storage.file'
+"""Static files collection method (defaults to copying files)."""
 
-# Accounts
-# ========
-#: Email address used as sender of account registration emails.
-SECURITY_EMAIL_SENDER = SUPPORT_EMAIL
-#: Email subject for account registration emails.
-SECURITY_EMAIL_SUBJECT_REGISTER = _(
-    "Welcome to Invenio App RDM!")
-#: Redis session storage URL.
+
+# Invenio-Accounts
+# ================
+# (Flask-Security, Flask-Login, Flask-Principal, Flask-KVSession)
+# See https://invenio-accounts.readthedocs.io/en/latest/configuration.html
+# See https://flask-security.readthedocs.io/en/3.0.0/configuration.html
+
 ACCOUNTS_SESSION_REDIS_URL = 'redis://localhost:6379/1'
-#: Enable session/user id request tracing. This feature will add X-Session-ID
-#: and X-User-ID headers to HTTP response. You MUST ensure that NGINX (or other
-#: proxies) removes these headers again before sending the response to the
-#: client. Set to False, in case of doubt.
+"""Redis session storage URL."""
+
 ACCOUNTS_USERINFO_HEADERS = True
+"""Enable session/user id request tracing.
 
-# Celery configuration
-# ====================
+This feature will add X-Session-ID and X-User-ID headers to HTTP response. You
+MUST ensure that NGINX (or other proxies) removes these headers again before
+sending the response to the client. Set to False, in case of doubt.
+"""
 
-BROKER_URL = 'amqp://guest:guest@localhost:5672/'
-#: URL of message broker for Celery (default is RabbitMQ).
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672/'
-#: URL of backend for result storage (default is Redis).
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/2'
-#: Scheduled tasks configuration (aka cronjobs).
+SECURITY_EMAIL_SENDER = "info@inveniosoftware.org"
+"""Email address used as sender of account registration emails."""
+
+SECURITY_EMAIL_SUBJECT_REGISTER = _("Welcome to Invenio App RDM!")
+"""Email subject for account registration emails."""
+
+
+# Invenio-Celery / Celery / Flask-Celeryext
+# =========================================
+# See https://invenio-celery.readthedocs.io/en/latest/configuration.html
+# See docs.celeryproject.org/en/latest/userguide/configuration.html
+# See https://flask-celeryext.readthedocs.io/en/latest/
+
+BROKER_URL = "amqp://guest:guest@localhost:5672/"
+"""URL of message broker for Celery 3 (default is RabbitMQ)."""
+
 CELERY_BEAT_SCHEDULE = {
     'indexer': {
         'task': 'invenio_indexer.tasks.process_bulk_queue',
@@ -109,49 +170,79 @@ CELERY_BEAT_SCHEDULE = {
     'accounts': {
         'task': 'invenio_accounts.tasks.clean_session_table',
         'schedule': timedelta(minutes=60),
-    },
+    }
 }
+"""Scheduled tasks configuration (aka cronjobs)."""
 
-# Database
-# ========
-#: Database URI including user and password
+CELERY_BROKER_URL = BROKER_URL
+"""Same as BROKER_URL to support Celery 4."""
+
+CELERY_RESULT_BACKEND = "redis://localhost:6379/2"
+"""URL of backend for result storage (default is Redis)."""
+
+
+# Flask-SQLAlchemy
+# ================
+# See https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
+
 SQLALCHEMY_DATABASE_URI = \
     'postgresql+psycopg2://invenio-app-rdm:invenio-app-rdm@localhost/' \
     'invenio-app-rdm'
+"""Database URI including user and password.
 
-# JSONSchemas
-# ===========
-#: Hostname used in URLs for local JSONSchemas.
-JSONSCHEMAS_HOST = '0.0.0.0'
-JSONSCHEMAS_ENDPOINT = '/schemas'
-JSONSCHEMAS_REGISTER_ENDPOINTS_UI = True
+Default value is provided to make module testing easier.
+"""
 
-# Flask configuration
+
+# Invenio-JSONSchemas
 # ===================
-# See details on
-# http://flask.pocoo.org/docs/0.12/config/#builtin-configuration-values
+# See https://invenio-jsonschemas.readthedocs.io/en/latest/configuration.html
 
-#: Secret key - each installation (dev, production, ...) needs a separate key.
-#: It should be changed before deploying.
-SECRET_KEY = 'CHANGE_ME'
-#: Max upload size for form data via application/mulitpart-formdata.
-MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MiB
-#: Sets cookie with the secure flag by default
-SESSION_COOKIE_SECURE = True
-#: Since HAProxy and Nginx route all requests no matter the host header
-#: provided, the allowed hosts variable is set to localhost. In production it
-#: should be set to the correct host and it is strongly recommended to only
-#: route correct hosts to the application.
-APP_ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+JSONSCHEMAS_HOST = '0.0.0.0'
+"""Hostname used in URLs for local JSONSchemas."""
+
 
 # OAI-PMH
 # =======
-OAISERVER_ID_PREFIX = 'oai:invenio-app-rdm.org:'
-# Debug
-# =====
-# Flask-DebugToolbar is by default enabled when the application is running in
-# debug mode. More configuration options are available at
-# https://flask-debugtoolbar.readthedocs.io/en/latest/#configuration
+# See https://github.com/inveniosoftware/invenio-oaiserver/blob/master/invenio_oaiserver/config.py  # noqa
+# (Using GitHub because documentation site out-of-sync at time of writing)
 
-#: Switches off incept of redirects by Flask-DebugToolbar.
+OAISERVER_ID_PREFIX = 'oai:invenio-app-rdm.org:'
+"""The prefix that will be applied to the generated OAI-PMH ids."""
+
+
+# Flask-DebugToolbar
+# ==================
+# See https://flask-debugtoolbar.readthedocs.io/en/latest/#configuration
+# Flask-DebugToolbar is by default enabled when the application is running in
+# debug mode. More configuration options are available above
+
 DEBUG_TB_INTERCEPT_REDIRECTS = False
+"""Switches off incept of redirects by Flask-DebugToolbar."""
+
+
+# Flask-Caching
+# =============
+# See https://flask-caching.readthedocs.io/en/latest/index.html#configuring-flask-caching  # noqa
+
+CACHE_REDIS_URL = "redis://localhost:6379/0"
+"""URL to connect to Redis server."""
+
+CACHE_TYPE = "redis"
+"""Use Redis caching object."""
+
+
+# Invenio-Search
+# ==============
+# See https://invenio-search.readthedocs.io/en/latest/configuration.html
+
+SEARCH_ELASTIC_HOSTS = [{"host": "localhost", "port": 9200}]
+"""Elasticsearch hosts."""
+
+
+# Invenio-Base
+# ============
+# See https://invenio-base.readthedocs.io/en/latest/api.html#invenio_base.wsgi.wsgi_proxyfix  # noqa
+
+WSGI_PROXIES = 2
+"""Correct number of proxies in front of your application."""
