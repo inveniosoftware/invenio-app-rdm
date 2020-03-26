f317460e2631a8c02c71d4b39df9c0397f9982f3
Guillaume Viger <fenekku@fenekku.com>
2020-03-26 12:23:23 -0500

    release: v0.7.0

--- invenio_app_rdm/version.py
+++ invenio_app_rdm/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University.
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -12,4 +12,4 @@ This file is imported by ``invenio_app_rdm.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = '0.6.3'
+__version__ = '0.7.0'
