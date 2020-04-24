402d59ec105006d158c1561b7a7bd34b222baae5
Guillaume Viger <fenekku@fenekku.com>
2020-04-24 16:21:41 -0500

    deposit: fix controller initialization

--- invenio_app_rdm/theme/assets/js/invenio_app_rdm/deposits/RDMDepositForm/RDMDepositApiController.js
+++ invenio_app_rdm/theme/assets/js/invenio_app_rdm/deposits/RDMDepositForm/RDMDepositApiController.js
@@ -2,6 +2,7 @@ import { DepositApiController } from "../../../react_invenio_deposit";
 
 export class RDMDepositApiController extends DepositApiController {
   constructor(apiClient) {
+    super(apiClient);
     console.log("RDM Deposit Controller");
   }
 }
