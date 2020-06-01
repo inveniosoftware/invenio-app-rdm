76467ff1ba0347cae6eb3f47ff94518599d3eefa
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-06-01 17:59:51 +0200

    theme: fix double importing of semantic ui theming
    - Fixes a problem were the RDM and instane CSS would be included twice
      due to the Semantic UI theming. Depends on
      inveniosoftware/cookiecutter-invenio-rdm#85. We no longer need a
      dynamically generated theme bundle and we no longer need
      INSTANCE_THEME_FILE. This role is taken over by theme.config which
      is provided by cookiecutter-invenio-rdm.
    
    - Removes use of the external font to not make external requests.
    
    - Simplifies templates to use just Invenio-Theme files.

--- invenio_app_rdm/theme/webpack.py
+++ invenio_app_rdm/theme/webpack.py
@@ -11,43 +11,37 @@
 from flask import current_app
 from invenio_assets.webpack import WebpackThemeBundle
 
-
-def theme():
-    """Returns module's webpack bundle.
-
-    This is a callable function in order to lazy load `current_app`
-    and avoid working outside application context.
-    """
-    return WebpackThemeBundle(
-        __name__,
-        'assets',
-        default='semantic-ui',
-        themes={
-            'semantic-ui': dict(
-                entry={
-                    'invenio-app-rdm-theme':
-                        current_app.config['INSTANCE_THEME_FILE'],
-                    'invenio-app-rdm-deposits-deposit-searchbar-js':
-                        './js/invenio_app_rdm/deposits/RDMDepositSearchBar/index.js',  # noqa
-                    'invenio-app-rdm-deposits-deposit-form-js':
-                        './js/invenio_app_rdm/deposits/RDMDepositForm/index.js',  # noqa
-                    'invenio-app-rdm-search-app-js':
-                        './js/invenio_app_rdm/search_app_customizations.js',
-                },
-                dependencies={
-                    # add any additional npm dependencies here...
-                    "@babel/runtime": "^7.9.0",
-                    'formik': '^2.1.4',
-                    'luxon': '^1.23.0',
-                    'path': '^0.12.7',
-                    'prop-types': '^15.7.2',
-                    'react-invenio-deposit': '^0.2.0',
-                    'react-invenio-forms': '^0.3.0',
-                },
-                aliases={
-                    '../../theme.config$': 'less/theme.config',
-                    '@templates': "templates",
-                }
-            ),
-        }
-    )
+theme = WebpackThemeBundle(
+    __name__,
+    'assets',
+    default='semantic-ui',
+    themes={
+        'semantic-ui': dict(
+            entry={
+                'invenio-app-rdm-deposits-deposit-searchbar-js':
+                    './js/invenio_app_rdm/deposits/RDMDepositSearchBar/index.js',  # noqa
+                'invenio-app-rdm-deposits-deposit-form-js':
+                    './js/invenio_app_rdm/deposits/RDMDepositForm/index.js',
+                'invenio-app-rdm-search-app-js':
+                    './js/invenio_app_rdm/search_app_customizations.js',
+            },
+            dependencies={
+                # add any additional npm dependencies here...
+                "@babel/runtime": "^7.9.0",
+                'formik': '^2.1.4',
+                'luxon': '^1.23.0',
+                'path': '^0.12.7',
+                'prop-types': '^15.7.2',
+                'react-invenio-deposit': '^0.2.0',
+                'react-invenio-forms': '^0.3.0',
+            },
+            aliases={
+                # Define Semantic-UI theme configuration needed by
+                # Invenio-Theme in order to build Semantic UI (in theme.js
+                # entry point). theme.config itself is provided by
+                # cookiecutter-invenio-rdm.
+                '../../theme.config$': 'less/theme.config',
+            }
+        ),
+    }
+)
