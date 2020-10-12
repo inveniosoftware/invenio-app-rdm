8dc95d9d98486dad9ef63c6372932f9fe23f1e10
Pablo Panero <pablo.panero@cern.ch>
2020-10-12 09:39:51 +0200

    dependencies: bump rdm-records

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ for name, reqs in extras_require.items():
 
 install_requires = [
     'invenio[base,auth,metadata,files]{}'.format(invenio_version),
-    'invenio-rdm-records>=0.20.7',
+    'invenio-rdm-records>=0.20.8',
 ]
 
 packages = find_packages()
