44827db421bd95dc2d7907e4983028e0f7cb36b7
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2021-02-05 17:57:32 +0100

    theme: refactor views
    * merge both blueprints into a single blueprint again, as that's easier
      to handle from a naming perspective (e.g. for url_for(...))
    * instead, split up the routes and filters into separate modules

--- setup.py
+++ setup.py
@@ -96,10 +96,7 @@ setup(
             'invenio-app-rdm = invenio_app.cli:cli',
         ],
         'invenio_base.blueprints': [
-            ("invenio_app_rdm = invenio_app_rdm.theme.views:"
-             "ui_blueprint"),
-            ("invenio_app_rdm_records = invenio_app_rdm.theme.views:"
-             "records_ui_blueprint"),
+            "invenio_app_rdm = invenio_app_rdm.theme.views:ui_blueprint",
         ],
         'invenio_assets.webpack': [
             'invenio_app_rdm_theme = invenio_app_rdm.theme.webpack:theme',
