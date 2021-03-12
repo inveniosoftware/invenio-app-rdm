badb0a1b84fa584f25c49875fad6a582d4dfe64f
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2021-03-12 13:53:40 +0100

    landing-page: make New version button green and move version link in bottom

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/NewVersionButton.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/NewVersionButton.js
@@ -8,27 +8,26 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Icon, Button } from "semantic-ui-react";
 
-
 export const NewVersionButton = (props) => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const recid = props.recid;
   const handleError = props.onError;
   const handleClick = () => {
     setLoading(true);
     axios
-    .post(`/api/records/${recid}/versions`)
-    .then((response) => {
-      const new_version_recid = response.data.id;
-      window.location = `/uploads/${new_version_recid}`;
-    })
-    .catch((error) => {
-      setLoading(false);
-      handleError(error.response.data.message);
-    });
+      .post(`/api/records/${recid}/versions`)
+      .then((response) => {
+        const new_version_recid = response.data.id;
+        window.location = `/uploads/${new_version_recid}`;
+      })
+      .catch((error) => {
+        setLoading(false);
+        handleError(error.response.data.message);
+      });
   };
 
   return (
-    <Button color="orange" size="mini" onClick={handleClick} loading={loading}>
+    <Button color="green" size="mini" onClick={handleClick} loading={loading}>
       <Icon name="tag" />
       New Version
     </Button>
