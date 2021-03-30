0c2bdce2de9a11beb92549871de244f373684258
Marco Vidal García <marco.vidal@cern.ch>
2021-03-30 17:40:22 +0200

    landing-page: add share link modal

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ShareButton.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ShareButton.js
@@ -4,22 +4,35 @@
 // Invenio RDM Records is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-import React from "react";
+import React, { useState } from "react";
 import { Icon, Button, Popup } from "semantic-ui-react";
+import { ShareModal } from "./ShareModal";
 
 export const ShareButton = (props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+
   return (
-    <Popup
-      content="You don't have permissions to share this record."
-      disabled={!props.disabled}
-      trigger={
-        <div>
-          <Button disabled={props.disabled} primary size="mini">
-            <Icon name="share square" />
-            Share
-          </Button>
-        </div>
-      }
-    />
+    <>
+      <Popup
+        content="You don't have permissions to share this record."
+        disabled={!props.disabled}
+        trigger={
+          <div>
+            <Button
+              onClick={handleOpen}
+              disabled={props.disabled}
+              primary
+              size="mini"
+            >
+              <Icon name="share square" />
+              Share
+            </Button>
+          </div>
+        }
+      />
+      <ShareModal open={modalOpen} handleClose={handleClose} />
+    </>
   );
 };
