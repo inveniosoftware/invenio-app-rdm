9f2e1563b20b1c75d290b418b192c88ad2394059
mb-wali <mb_wali@hotmail.com>
2021-07-19 13:29:32 +0200

    i18n: adds german translations i18n: convert htmltag to trans tags i18n: wraps components with I18nextProvider

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/index.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/index.js
@@ -8,16 +8,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
-
+import { I18nextProvider } from 'react-i18next';
+import { i18next } from "@translations/invenio_app_rdm/i18next";
 import { getInputFromDOM } from "react-invenio-deposit";
 import { RDMDepositForm } from "./RDMDepositForm";
 
+
 ReactDOM.render(
+  <I18nextProvider i18n={i18next}>
   <RDMDepositForm
     record={getInputFromDOM("deposits-record")}
     files={getInputFromDOM("deposits-record-files")}
     config={getInputFromDOM("deposits-config")}
     permissions={getInputFromDOM("deposits-record-permissions")}
-  />,
+  /></I18nextProvider>,
   document.getElementById("deposit-form")
 );
