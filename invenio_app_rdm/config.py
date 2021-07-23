3a0f8cc8ad99d2671413cb3e1f44ea24b1eeae12
Pablo Panero <pablo.panero@cern.ch>
2021-07-23 11:23:41 +0200

    config: fix iiif template preview path

--- invenio_app_rdm/config.py
+++ invenio_app_rdm/config.py
@@ -174,8 +174,11 @@ FILES_REST_PERMISSION_FACTORY = files_rest_permission_factory
 # =================
 # See https://invenio-iiif.readthedocs.io/en/latest/configuration.html
 
-IIIF_PREVIEW_TEMPLATE = "invenio_app_rdm/landing_page/iiif_preview.html"
+IIIF_PREVIEW_TEMPLATE = "invenio_app_rdm/records/iiif_preview.html"
 """Template for IIIF image preview."""
+
+IIIF_API_DECORATOR_HANDLER = None
+IIIF_IMAGE_OPENER_HANDLER = "invenio_rdm_records.resources.iiif:image_opener"
 
 # Invenio-Previewer
 # =================
@@ -513,13 +516,6 @@ the values should be set (see invenio-records.dictutils).
 If the value is callable, its return value will be used for the field
 (e.g. lambda/function for dynamic calculation of values).
 """
-
-
-# Invenio-IIIF
-# ===================
-
-IIIF_API_DECORATOR_HANDLER = None
-IIIF_IMAGE_OPENER_HANDLER = "invenio_rdm_records.resources.iiif:image_opener"
 
 RDM_CITATION_STYLES = [
      ('apa', _('APA')),
