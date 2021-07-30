c17d4326cd1b9ac5ba01732de2712014be4f31c6
Pablo Panero <pablo.panero@cern.ch>
2021-07-30 09:23:32 +0200

    release: v6.0.0.dev1

--- invenio_app_rdm/version.py
+++ invenio_app_rdm/version.py
@@ -20,4 +20,4 @@ This file is imported by ``invenio_app_rdm.__init__``, and parsed by
 #
 # See PEP 0440 for details - https://www.python.org/dev/peps/pep-0440
 
-__version__ = '6.0.0.dev0'
+__version__ = '6.0.0.dev1'
