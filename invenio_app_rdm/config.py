08355ec6264ed271fef0b1afe68a7f221c72da45
Pablo Panero <pablo.panero@cern.ch>
2021-08-03 14:43:43 +0200

    config: disable bulk indexing task

--- invenio_app_rdm/config.py
+++ invenio_app_rdm/config.py
@@ -263,10 +263,10 @@ BROKER_URL = "amqp://guest:guest@localhost:5672/"
 """URL of message broker for Celery 3 (default is RabbitMQ)."""
 
 CELERY_BEAT_SCHEDULE = {
-    'indexer': {
-        'task': 'invenio_indexer.tasks.process_bulk_queue',
-        'schedule': timedelta(minutes=5),
-    },
+    # 'indexer': {
+    #     'task': 'invenio_indexer.tasks.process_bulk_queue',
+    #     'schedule': timedelta(minutes=5),
+    # },
     'accounts': {
         'task': 'invenio_accounts.tasks.clean_session_table',
         'schedule': timedelta(minutes=60),
