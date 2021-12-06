6fab03e7f155d10f011acc5b9cb1795f23b9ed08
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-12-06 09:40:34 +0100

    installation: bump invenio-rdm-records

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ install_requires = [
     'CairoSVG>=2.5.2,<3.0.0',
     f'invenio[base,auth,metadata,files]{invenio_version}',
     'invenio-logging[sentry-sdk]>=1.3.0,<1.4.0',
-    'invenio-rdm-records>=0.33.9,<0.34.0',
+    'invenio-rdm-records>=0.33.10,<0.34.0',
 ]
 
 packages = find_packages()
