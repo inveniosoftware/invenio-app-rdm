7c8d97803d106d393e7a38f26a5ac989fe35ee79
Nicola Tarocco <ntarocco@gmail.com>
2022-02-11 11:05:42 +0100

    webpack: upgrade npm deps

--- invenio_app_rdm/theme/webpack.py
+++ invenio_app_rdm/theme/webpack.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019-2021 CERN.
-# Copyright (C) 2019-2021 Northwestern University.
-# Copyright (C)      2021 TU Wien.
+# Copyright (C) 2019-2022 CERN.
+# Copyright (C) 2019-2022 Northwestern University.
+# Copyright (C)      2022 TU Wien.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -28,24 +28,23 @@ theme = WebpackThemeBundle(
                 './js/invenio_app_rdm/user_dashboard/index.js',
             },
             dependencies={
-                # add any additional npm dependencies here...
-                "@babel/runtime": "^7.9.0",
-                'formik': '^2.1.4',
+                '@babel/runtime': '^7.9.0',
+                '@ckeditor/ckeditor5-build-classic': '^16.0.0',
+                '@ckeditor/ckeditor5-react': '^2.1.0',
+                'formik': '^2.1.0',
+                "i18next": "^20.3.0",
+                "i18next-browser-languagedetector": "^6.1.0",
                 'luxon': '^1.23.0',
                 'path': '^0.12.7',
                 'prop-types': '^15.7.2',
-                'react-dnd': '^11.1.3',
-                'react-dnd-html5-backend': '^11.1.3',
-                'react-invenio-deposit': '^0.17.2',
+                'react-copy-to-clipboard': '^5.0.0',
+                'react-dnd': '^11.1.0',
+                'react-dnd-html5-backend': '^11.1.0',
+                'react-dropzone': "^11.0.0",
+                "react-i18next": "^11.11.0",
+                'react-invenio-deposit': '^0.17.0',
                 'react-invenio-forms': '^0.9.0',
-                'react-dropzone': "^11.0.3",
-                'yup': '^0.32.9',
-                '@ckeditor/ckeditor5-build-classic': '^16.0.0',
-                '@ckeditor/ckeditor5-react': '^2.1.0',
-                "i18next": "^20.3.1",
-                "react-i18next": "^11.11.3",
-                "i18next-browser-languagedetector": "^6.1.1",
-                'react-copy-to-clipboard': '^5.0.3'
+                'yup': '^0.32.0',
             },
             aliases={
                 # Define Semantic-UI theme configuration needed by
