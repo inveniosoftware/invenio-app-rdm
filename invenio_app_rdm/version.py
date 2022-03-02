1eb585313f812cacb1866acaecc3b6421c349bdb
Nicola Tarocco <ntarocco@gmail.com>
2022-03-02 10:08:15 +0100

    release: v8.0.0.dev6

--- invenio_app_rdm/version.py
+++ invenio_app_rdm/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2021 CERN.
-# Copyright (C) 2020-2021 Northwestern University.
+# Copyright (C) 2020-2022 CERN.
+# Copyright (C) 2020-2022 Northwestern University.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -20,4 +20,4 @@ This file is imported by ``invenio_app_rdm.__init__``, and parsed by
 #
 # See PEP 0440 for details - https://www.python.org/dev/peps/pep-0440
 
-__version__ = '8.0.0.dev5'
+__version__ = '8.0.0.dev6'
