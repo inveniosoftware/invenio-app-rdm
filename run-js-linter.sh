7bf8fd549e1c7f21f1db8f273cc8740b2d52256f
jrcastro2 <jrcastro9515@gmail.com>
2022-03-09 15:46:44 +0100

    eslint: add eslint rules to js files

---
+++ run-js-linter.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+# Usage:
+#   ./run-js-linter.sh [args]
+
+# Arguments
+# -i|--install: installs eslint-config-invenio
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+for arg in $@; do
+	case ${arg} in
+		-i|--install)
+			npm install @inveniosoftware/eslint-config-invenio@^1.0.0;;
+		*)
+			printf "Argument ${RED}$arg${NC} not supported\n"
+			exit;;
+	esac
+done
+
+printf "${GREEN}Run eslint${NC}\n"
+npx eslint -c .eslintrc.yml invenio_app_rdm/**/*.js
