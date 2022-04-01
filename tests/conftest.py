618b52c39873d9fd26b05fc0269e003d45b6f17d
Nicola Tarocco <ntarocco@gmail.com>
2022-04-01 11:29:46 +0200

    global: support Flask v2.1

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,22 @@
 
 """Common pytest fixtures and plugins."""
 
+# Monkey patch Werkzeug 2.1
+# Flask-Login uses the safe_str_cmp method which has been removed in Werkzeug
+# 2.1. Flask-Login v0.6.0 (yet to be released at the time of writing) fixes the
+# issue. Once we depend on Flask-Login v0.6.0 as the minimal version in
+# Flask-Security-Invenio/Invenio-Accounts we can remove this patch again.
+try:
+    # Werkzeug <2.1
+    from werkzeug import security
+    security.safe_str_cmp
+except AttributeError:
+    # Werkzeug >=2.1
+    import hmac
+
+    from werkzeug import security
+    security.safe_str_cmp = hmac.compare_digest
+
 from collections import namedtuple
 
 import pytest
