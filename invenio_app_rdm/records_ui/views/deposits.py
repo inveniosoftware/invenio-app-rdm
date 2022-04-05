5b97ab0aaa78f7f356965d2f4cefb82dee70a06b
David Eckhard <dabombeeer@gmail.com>
2022-04-05 16:35:21 +0200

    config: add deposit form quota config

--- invenio_app_rdm/records_ui/views/deposits.py
+++ invenio_app_rdm/records_ui/views/deposits.py
@@ -260,6 +260,7 @@ def get_form_config(**kwargs):
         current_locale=str(current_i18n.locale),
         default_locale=current_app.config.get('BABEL_DEFAULT_LOCALE', 'en'),
         pids=get_form_pids_config(),
+        quota=current_app.config.get('APP_RDM_DEPOSIT_FORM_QUOTA'),
         **kwargs
     )
 
