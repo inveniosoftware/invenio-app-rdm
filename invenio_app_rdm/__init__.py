539740f70848ebe672d671cfbbdfd9e010992d3c
Nicola Tarocco <ntarocco@gmail.com>
2022-04-19 11:28:15 +0200

    release: v9.0.0.dev9

--- invenio_app_rdm/__init__.py
+++ invenio_app_rdm/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University.
+# Copyright (C) 2019-2022 CERN.
+# Copyright (C) 2019-2022 Northwestern University.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Invenio App RDM."""
 
-__version__ = '9.0.0.dev8'
+__version__ = '9.0.0.dev9'
 
 __all__ = ('__version__', )
