5a7f547cb238adc038ea740915d451e244bc5e5d
nico <nicolas.rodriguezcuellar@student.hu.nl>
2022-04-20 17:03:06 +0200

    request views: reroute request views to general view
    - added a general view which will handle what template to return based on the request
    - moved the request routes to app-rdm from communitites and requests

--- invenio_app_rdm/config.py
+++ invenio_app_rdm/config.py
@@ -677,6 +677,9 @@ RDM_SEARCH_USER_REQUESTS = {
 
 RDM_REQUESTS_ROUTES = {
     'user-dashboard-community-submission': '/me/requests/<pid_value>',
+    'invitation_details':
+        '/communities/<community_pid_value>/invitations/<pid_value>',
+    'request_details': '/requests/<pid_value>'
 }
 
 RDM_COMMUNITIES_ROUTES = {
