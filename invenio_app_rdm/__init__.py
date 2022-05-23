f71bf300aee990f047cbee3e3443f1825dacd230
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2022-05-23 13:16:53 +0200

    release: v9.0.0rc3

--- invenio_app_rdm/__init__.py
+++ invenio_app_rdm/__init__.py
@@ -8,6 +8,14 @@
 
 """Invenio App RDM."""
 
-__version__ = '9.0.0.rc2'
+# Note, that dev versions must have an extra dot compared to alpha/beta/rc
+# versions in the version string. Examples:
+#
+# Correct: "1.0.0.dev0" and "1.0.0a1" and "1.0.0rc1"
+# Incorrect: "1.0.0dev0" and "1.0.0.a1" and "1.0.0.rc1"
+#
+# See PEP 0440 for details - https://www.python.org/dev/peps/pep-0440
+
+__version__ = '9.0.0rc3'
 
 __all__ = ('__version__', )
