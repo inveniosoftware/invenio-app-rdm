5bc558654d3e0af6d93e3a72ebd7f92c68c98cc1
pablo <pineirin@gmail.com>
2022-11-08 17:52:16 +0100

    Communities dashboard: add data to the search results
    * closes: zenodo/zenodo-rdm#48

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/communities_items/ComputerTabletCommunitiesItem.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/communities_items/ComputerTabletCommunitiesItem.js
@@ -7,7 +7,8 @@
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 import React from "react";
 import { Image } from "react-invenio-forms";
-import { Button, Icon, Item, Label } from "semantic-ui-react";
+import { Button, Icon, Item, Label, Grid } from "semantic-ui-react";
+import { DateTime } from "luxon";
 import PropTypes from "prop-types";
 
 export const ComputerTabletCommunitiesItem = ({ result, index }) => {
@@ -23,48 +24,66 @@ export const ComputerTabletCommunitiesItem = ({ result, index }) => {
         wrapped
         src={result.links.logo}
         size="tiny"
-        className="community-logo mt-auto mb-auto"
+        className="community-logo rel-mt-1"
       />
       <Item.Content>
-        <Item.Extra className="user-communities">
-          {communityType && (
-            <Label size="tiny" className="primary">
-              <Icon name="tag" />
-              {communityType}
-            </Label>
-          )}
-          <Label size="tiny" className={visibilityColor}>
-            {visibilityIcon && <Icon name={visibilityIcon} />}
-            {visibilityText}
-          </Label>
-          <Button
-            compact
-            size="small"
-            floated="right"
-            href={`/communities/${result.id}/settings`}
-            labelPosition="left"
-            icon="edit"
-            content={i18next.t("Edit")}
-          />
-        </Item.Extra>
-        <Item.Header as="h2">
-          <a href={`/communities/${result.id}`}>{result.metadata.title}</a>
-        </Item.Header>
-        <Item.Meta>
-          <div
-            className="truncate-lines-2"
-            dangerouslySetInnerHTML={{
-              __html: result.metadata.description,
-            }}
-          />
-        </Item.Meta>
-        <Item>
-          {result.metadata.website && (
-            <a href={result.metadata.website} target="_blank" rel="noopener noreferrer">
-              {result.metadata.website}
-            </a>
-          )}
-        </Item>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={13}>
+              <Item.Extra className="user-communities">
+                {communityType && (
+                  <Label size="tiny" className="primary">
+                    <Icon name="tag" />
+                    {communityType}
+                  </Label>
+                )}
+                <Label size="tiny" className={visibilityColor}>
+                  {visibilityIcon && <Icon name={visibilityIcon} />}
+                  {visibilityText}
+                </Label>
+              </Item.Extra>
+              <Item.Header as="h2">
+                <a href={`/communities/${result.id}`}>{result.metadata.title}</a>
+              </Item.Header>
+              <Item.Meta>
+                <div
+                  className="truncate-lines-2"
+                  dangerouslySetInnerHTML={{
+                    __html: result.metadata.description,
+                  }}
+                />
+              </Item.Meta>
+              <Item>
+                {result.metadata.website && (
+                  <a
+                    href={result.metadata.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {result.metadata.website}
+                  </a>
+                )}
+              </Item>
+            </Grid.Column>
+            <Grid.Column width={3} className="flex column">
+              <Item.Extra className="text-align-right">
+                <Button
+                  compact
+                  size="small"
+                  href={`/communities/${result.id}/settings`}
+                  className="mt-0"
+                  labelPosition="left"
+                  icon="edit"
+                  content={i18next.t("Edit")}
+                />
+              </Item.Extra>
+              <Item.Extra className="text-align-right mt-auto">
+                {i18next.t("Created: ")}
+                {DateTime.fromISO(result.created).toLocaleString(i18next.language)}
+              </Item.Extra>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Item.Content>
     </Item>
   );
