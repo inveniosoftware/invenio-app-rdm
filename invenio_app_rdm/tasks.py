ae90c1f7b8ea5aef96b241465edd0a6aa74aeecb
Manuel Alejandro <mumox7@gmail.com>
2022-11-22 12:22:42 +0000

    tasks: migrated checksum and file report tasks

---
+++ invenio_app_rdm/tasks.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""Celery tasks for invenio-app-rdm."""
+
+import sqlalchemy as sa
+from celery import shared_task
+from invenio_db import db
+from invenio_files_rest.models import FileInstance
+
+from .utils.files import send_integrity_report_email
+
+
+@shared_task()
+def file_integrity_report():
+    """Send a report of uhealthy/missing files to system admins."""
+    # First retry verifying files that errored during their last check
+    files = FileInstance.query.filter(FileInstance.last_check.is_(None))
+    for f in files:
+        try:
+            f.clear_last_check()
+            db.session.commit()
+            f.verify_checksum(throws=False)
+            db.session.commit()
+        except Exception:
+            pass  # Don't fail sending the report in case of some file error
+
+    unhealthy_files = (
+        FileInstance.query.filter(
+            sa.or_(
+                FileInstance.last_check.is_(None), FileInstance.last_check.is_(False)
+            )
+        )
+        .order_by(FileInstance.created.desc())
+        .all()
+    )
+
+    send_integrity_report_email(unhealthy_files)
---
+++ tests/test_tasks.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""Test invenio-app-rdm celery tasks."""
+
+
+from invenio_app_rdm.tasks import file_integrity_report
+
+
+def test_task_file_integrity_report(app, invalid_file_instance):
+    """Test celery task for file integrity reports."""
+    assert invalid_file_instance.last_check is False
+
+    # A report must be generated for the file
+    mail = app.extensions.get("mail")
+    assert mail
+
+    recipients = "test@invenio.org"
+    sender = "test@invenio.org"
+
+    file_name = invalid_file_instance.objects[0].key
+    uri = invalid_file_instance.uri
+    file_id = invalid_file_instance.id
+    file_name = invalid_file_instance.objects[0].key
+    checksum = invalid_file_instance.checksum
+
+    with mail.record_messages() as outbox:
+        # Configure email and validate that email was sent
+        app.config["FILES_INTEGRITY_REPORT_TEMPLATE"] = "mock_mail.html"
+        app.config["ADMIN_EMAIL_RECIPIENT"] = recipients
+        app.config["MAIL_DEFAULT_SENDER"] = sender
+
+        file_integrity_report()
+        assert len(outbox) == 1
+        mail_sent = outbox[0]
+        assert mail_sent.recipients == [recipients]
+        assert mail_sent.sender == sender
+        assert "Checksum: {}".format(checksum) in mail_sent.body
+        assert "URI: {}".format(uri) in mail_sent.body
+        assert "ID: {}".format(str(file_id)) in mail_sent.body
+        assert "Name: {}".format(file_name) in mail_sent.body
+
+
+def test_integrity_report_invalid_template(app, invalid_file_instance):
+    """Test non-existant e-mail template."""
+    assert invalid_file_instance.last_check is False
+
+    mail = app.extensions.get("mail")
+    assert mail
+
+    with mail.record_messages() as outbox:
+        # Remove the template, no e-mail is sent
+        app.config["FILES_INTEGRITY_REPORT_TEMPLATE"] = None
+        file_integrity_report()
+        assert len(outbox) == 0
+
+
+def test_integrity_report_invalid_addresses(app, invalid_file_instance):
+    """Test invalid recipient address."""
+    assert invalid_file_instance.last_check is False
+
+    mail = app.extensions.get("mail")
+    assert mail
+
+    with mail.record_messages() as outbox:
+        # Use mock template
+        app.config["FILES_INTEGRITY_REPORT_TEMPLATE"] = "mock_mail.html"
+
+        # Recipient is not set, mail is not sent.
+        app.config["ADMIN_EMAIL_RECIPIENT"] = None
+        file_integrity_report()
+        assert len(outbox) == 0
+
+
+def test_integrity_report_default_template(app, invalid_file_instance):
+    """Test invalid recipient address."""
+    assert invalid_file_instance.last_check is False
+
+    mail = app.extensions.get("mail")
+    assert mail
+
+    recipients = "test@invenio.org"
+    sender = "test@invenio.org"
+
+    uri = invalid_file_instance.uri
+    file_id = invalid_file_instance.id
+    file_name = invalid_file_instance.objects[0].key
+    checksum = invalid_file_instance.checksum
+
+    with mail.record_messages() as outbox:
+        # Use default template
+        app.config["ADMIN_EMAIL_RECIPIENT"] = recipients
+        app.config["MAIL_DEFAULT_SENDER"] = sender
+        file_integrity_report()
+        assert len(outbox) == 1
+        mail_sent = outbox[0]
+        assert mail_sent.recipients == [recipients]
+        assert mail_sent.sender == sender
+        assert "Checksum: {}".format(checksum) in mail_sent.body
+        assert "URI: {}".format(uri) in mail_sent.body
+        assert "ID: {}".format(str(file_id)) in mail_sent.body
+        assert "Name: {}".format(file_name) in mail_sent.body
