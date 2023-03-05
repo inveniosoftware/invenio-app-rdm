9d8aa252e5d689884baa84dd5a3ec3909223e703
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-03-05 14:43:05 +0100

    assets: move in community records search
    * move the community records search components from invenio_communities

---
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/communityRecordsSearch/components.js
@@ -0,0 +1,3 @@
+export { CommunityRecordsSearchAppLayout } from "./CommunityRecordsSearchAppLayout";
+export { CommunityRecordsSearchBarElement } from "./CommunityRecordsSearchBarElement";
+export { CommunityRecordsSingleSearchBarElement } from "./CommunityRecordsSingleSearchBarElement";
