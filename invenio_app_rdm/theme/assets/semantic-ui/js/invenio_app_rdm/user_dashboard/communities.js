f0e853c56d7ca234d1d199b315c6dabc4a8c073f
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-03-05 13:59:03 +0100

    assets: remove redundant communities component
    * import the components from invnenio_communities

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/communities.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/communities.js
@@ -10,12 +10,14 @@
 import { createSearchAppInit } from "@js/invenio_search_ui";
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 import React from "react";
-import { Button, Card, Header, Icon, Segment } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+import {
+  CommunityItem,
+  ResultsGridItemTemplate,
+  CommunitiesEmptySearchResults,
+} from "@js/invenio_communities/community";
 import { RDMRecordSearchBarElement } from "../search/components";
 import { DashboardResultView, DashboardSearchLayoutHOC } from "./base";
-import { ComputerTabletCommunitiesItem } from "./communities_items/ComputerTabletCommunitiesItem";
-import { MobileCommunitiesItem } from "./communities_items/MobileCommunitiesItem";
-import PropTypes from "prop-types";
 import {
   ContribSearchAppFacets,
   ContribBucketAggregationElement,
@@ -24,47 +26,6 @@ import {
 import { overrideStore, parametrize } from "react-overridable";
 
 export const appName = "InvenioAppRdm.DashboardCommunities";
-
-function ResultsGridItemTemplate({ result, index }) {
-  return (
-    <Card fluid key={index} href={`/communities/${result.metadata.id}`}>
-      <Card.Content>
-        <Card.Header>{result.metadata.title}</Card.Header>
-        <Card.Description>
-          <div
-            className="truncate-lines-2"
-            dangerouslySetInnerHTML={{
-              __html: result.metadata.description,
-            }}
-          />
-        </Card.Description>
-      </Card.Content>
-    </Card>
-  );
-}
-
-ResultsGridItemTemplate.propTypes = {
-  result: PropTypes.object.isRequired,
-  index: PropTypes.string.isRequired,
-};
-
-export function CommunitiesResultsItemTemplate({ result, index }) {
-  return (
-    <>
-      <ComputerTabletCommunitiesItem result={result} index={index} />
-      <MobileCommunitiesItem result={result} index={index} />
-    </>
-  );
-}
-
-CommunitiesResultsItemTemplate.propTypes = {
-  result: PropTypes.object.isRequired,
-  index: PropTypes.string,
-};
-
-CommunitiesResultsItemTemplate.defaultProps = {
-  index: null,
-};
 
 export const DashboardCommunitiesSearchLayout = DashboardSearchLayoutHOC({
   searchBarPlaceholder: i18next.t("Search in my communities..."),
@@ -80,28 +41,6 @@ export const DashboardCommunitiesSearchLayout = DashboardSearchLayoutHOC({
   appName: appName,
 });
 
-export const RDMCommunitiesEmptyResults = (props) => {
-  const { queryString, resetQuery } = props;
-  return (
-    <Segment placeholder textAlign="center">
-      <Header icon>
-        <Icon name="search" />
-        {i18next.t("No communities found!")}
-      </Header>
-      {queryString && (
-        <Button primary onClick={() => resetQuery()}>
-          {i18next.t("Reset search")}
-        </Button>
-      )}
-    </Segment>
-  );
-};
-
-RDMCommunitiesEmptyResults.propTypes = {
-  queryString: PropTypes.string.isRequired,
-  resetQuery: PropTypes.func.isRequired,
-};
-
 const DashboardResultViewWAppName = parametrize(DashboardResultView, {
   appName: appName,
 });
@@ -109,10 +48,10 @@ const DashboardResultViewWAppName = parametrize(DashboardResultView, {
 export const defaultComponents = {
   [`${appName}.BucketAggregation.element`]: ContribBucketAggregationElement,
   [`${appName}.BucketAggregationValues.element`]: ContribBucketAggregationValuesElement,
-  [`${appName}.EmptyResults.element`]: RDMCommunitiesEmptyResults,
-  [`${appName}.ResultsList.item`]: CommunitiesResultsItemTemplate,
-  [`${appName}.ResultsGrid.item`]: ResultsGridItemTemplate,
+  [`${appName}.EmptyResults.element`]: CommunitiesEmptySearchResults,
   [`${appName}.SearchApp.facets`]: ContribSearchAppFacets,
+  [`${appName}.ResultsList.item`]: CommunityItem,
+  [`${appName}.ResultsGrid.item`]: ResultsGridItemTemplate,
   [`${appName}.SearchApp.layout`]: DashboardCommunitiesSearchLayout,
   [`${appName}.SearchApp.results`]: DashboardResultViewWAppName,
   [`${appName}.SearchBar.element`]: RDMRecordSearchBarElement,
