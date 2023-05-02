17fd047ffcb8b2cd8296bc7d81f791b470883e5f
Javier Romero Castro <jrcastro9515@gmail.com>
2023-05-02 16:13:36 +0200

    communities: add permission check to display create button
    * closes https://github.com/CERNDocumentServer/cds-rdm/issues/55

--- invenio_app_rdm/users_ui/views/dashboard.py
+++ invenio_app_rdm/users_ui/views/dashboard.py
@@ -10,6 +10,7 @@
 
 from flask import g, render_template
 from flask_login import current_user, login_required
+from invenio_communities.proxies import current_communities
 from invenio_users_resources.proxies import current_user_resources
 
 from invenio_app_rdm.records_ui.views.deposits import get_search_url
@@ -47,8 +48,12 @@ def communities():
     url = current_user_resources.users_service.links_item_tpl.expand(
         g.identity, current_user
     )["avatar"]
+    can_create_community = current_communities.service.check_permission(
+        g.identity, "create"
+    )
     return render_template(
         "invenio_app_rdm/users/communities.html",
         searchbar_config=dict(searchUrl=get_search_url()),
         user_avatar=url,
+        can_create_community=can_create_community,
     )
