5e898c0ba8a47ad4e520673034b2300f301ce951
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-05-31 17:47:55 +0200

    details: add access request form to restricted records' landing pages
    Co-authored-by Javier Romero Castro <jrcastro9515@gmail.com>

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/index.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/index.js
@@ -2,6 +2,7 @@
 // Copyright (C) 2020-2021 CERN.
 // Copyright (C) 2020-2021 Northwestern University.
 // Copyright (C) 2021 Graz University of Technology.
+// Copyright (C) 2023 TU Wien.
 //
 // Invenio RDM Records is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
