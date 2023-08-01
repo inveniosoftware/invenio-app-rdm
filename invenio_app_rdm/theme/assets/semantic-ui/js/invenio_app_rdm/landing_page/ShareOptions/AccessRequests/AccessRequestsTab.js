1cf9080359ca3465e47668dad6c2dee13e6d4324
anikachurilova <nyuta.churilova@gmail.com>
2023-08-01 10:22:45 +0200

    access request: add ui for secret link expiration access setting
    * closes https://github.com/inveniosoftware/invenio-rdm-records/issues/1387

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ShareOptions/AccessRequests/AccessRequestsTab.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ShareOptions/AccessRequests/AccessRequestsTab.js
@@ -10,7 +10,13 @@ import PropTypes from "prop-types";
 import { Formik } from "formik";
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 import { SuccessIcon } from "@js/invenio_communities/members";
-import { RadioField, TextAreaField, http, withCancel } from "react-invenio-forms";
+import {
+  RadioField,
+  TextAreaField,
+  http,
+  withCancel,
+  SelectField,
+} from "react-invenio-forms";
 import * as Yup from "yup";
 import _get from "lodash/get";
 
@@ -20,6 +26,7 @@ export class AccessRequestsTab extends Component {
     this.state = {
       loading: false,
       error: undefined,
+      actionSuccess: undefined,
     };
   }
 
@@ -31,7 +38,16 @@ export class AccessRequestsTab extends Component {
     allow_user_requests: Yup.bool(),
     allow_guest_requests: Yup.bool(),
     accept_conditions_text: Yup.string().nullable(),
+    secret_link_expiration: Yup.number(),
   });
+
+  linkExpirationOptions = [
+    { key: 0, text: i18next.t("Never"), value: 0 },
+    { key: 30, text: i18next.t("In 1 month"), value: 30 },
+    { key: 60, text: i18next.t("In 2 months"), value: 60 },
+    { key: 180, text: i18next.t("In 6 months"), value: 180 },
+    { key: 365, text: i18next.t("In 1 year"), value: 365 },
+  ];
 
   handleSubmit = async (values) => {
     this.setState({ loading: true, actionSuccess: false });
@@ -40,6 +56,7 @@ export class AccessRequestsTab extends Component {
       allow_user_requests: values.allow_user_requests,
       allow_guest_requests: values.allow_guest_requests,
       accept_conditions_text: values.accept_conditions_text,
+      secret_link_expiration: values.secret_link_expiration,
     };
 
     this.cancellableAction = withCancel(
@@ -80,6 +97,8 @@ export class AccessRequestsTab extends Component {
             allow_guest_requests: record.parent.access.settings.allow_guest_requests,
             accept_conditions_text:
               record.parent.access.settings.accept_conditions_text,
+            secret_link_expiration:
+              record.parent.access.settings.secret_link_expiration || 0,
           }}
           validationSchema={this.accessRequestSchema}
         >
@@ -124,9 +143,9 @@ export class AccessRequestsTab extends Component {
                         </Form.Field>
                         <label className="helptext mb-0 mt-10">
                           {i18next.t(
-                            "Enable users and guests to request access to your record. You will get an email " +
-                              "asking for approval. After you approve a request, users will be granted access and " +
-                              "guests will receive a secret link."
+                            "Enable users and guests to request access to your record. When access is requested " +
+                              "by someone, you will get an e-mail asking for approval. After you approve a request, " +
+                              "users will be granted access and guests will receive a secret link."
                           )}
                         </label>
                       </Grid.Column>
@@ -136,10 +155,33 @@ export class AccessRequestsTab extends Component {
                         <Form.Field>
                           <TextAreaField
                             placeholder={i18next.t(
-                              "Optional. Specify conditions under which you approve access."
+                              "Optional. Specify conditions under which you approve access. This message will be " +
+                                "visible for any user when requesting access to this record."
                             )}
                             fieldPath="accept_conditions_text"
                             rows={6}
+                          />
+                        </Form.Field>
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <h5>{i18next.t("Advanced options")}</h5>
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column width={3}>
+                        <Form.Field>
+                          <SelectField
+                            label={i18next.t("Link expiration")}
+                            fieldPath="secret_link_expiration"
+                            options={this.linkExpirationOptions}
+                            onChange={({ data, formikProps }) => {
+                              formikProps.form.setFieldValue(
+                                "secret_link_expiration",
+                                data.value
+                              );
+                            }}
                           />
                         </Form.Field>
                       </Grid.Column>
@@ -148,6 +190,7 @@ export class AccessRequestsTab extends Component {
                 </Form>
                 <Grid.Row>
                   <Grid.Column>
+                    <br />
                     <Button
                       size="small"
                       labelPosition="left"
