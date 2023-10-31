cf8ecfaddc10449c8aaa72177ee37cef54beed50
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2023-10-31 16:47:47 +0100

    communities: rename search tab to records.

--- invenio_app_rdm/communities_ui/views/ui.py
+++ invenio_app_rdm/communities_ui/views/ui.py
@@ -87,7 +87,7 @@ def create_ui_blueprint(app):
         communities = current_menu.submenu("communities")
         communities.submenu("search").register(
             "invenio_app_rdm_communities.communities_detail",
-            text=_("Search"),
+            text=_("Records"),
             order=1,
             expected_args=["pid_value"],
             **dict(icon="search", permissions=True),
