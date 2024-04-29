db18e345d430e1173bbe74da94e9c0faf92edcf1
Pablo Tamarit <pablo.tamarit@cern.ch>
2024-04-29 13:56:47 +0200

    linter: disable react/no-array-index-key for records list placeholder

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/frontpage/RecordsList.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/frontpage/RecordsList.js
@@ -69,6 +69,7 @@ export class RecordsList extends Component {
       <Container>
         <Header as="h2">{title}</Header>
         {Array.from(Array(10)).map((item, index) => (
+          // eslint-disable-next-line react/no-array-index-key
           <div key={index}>
             <Placeholder fluid className="rel-mt-3">
               <Placeholder.Header>
