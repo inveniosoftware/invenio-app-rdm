50456fdcd07ef303c86d2401a8842b13683b4d98
Pablo Tamarit <pablo.tamarit@cern.ch>
2024-05-27 09:59:01 +0200

    config: vocabularies Datastream awards readers

--- invenio_app_rdm/config.py
+++ invenio_app_rdm/config.py
@@ -97,6 +97,9 @@ from invenio_vocabularies.config import (
     VOCABULARIES_DATASTREAM_WRITERS,
 )
 from invenio_vocabularies.contrib.awards.datastreams import (
+    VOCABULARIES_DATASTREAM_READERS as AWARDS_READERS,
+)
+from invenio_vocabularies.contrib.awards.datastreams import (
     VOCABULARIES_DATASTREAM_TRANSFORMERS as AWARDS_TRANSFORMERS,
 )
 from invenio_vocabularies.contrib.awards.datastreams import (
@@ -687,6 +690,7 @@ VOCABULARIES_DATASTREAM_READERS = {
     **VOCABULARIES_DATASTREAM_READERS,
     **NAMES_READERS,
     **COMMON_ROR_READERS,
+    **AWARDS_READERS,
 }
 """Data Streams readers."""
 
