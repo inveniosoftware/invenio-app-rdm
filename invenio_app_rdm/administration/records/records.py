a0b335829c6798e08b79b88708539f3e3ca53240
yashlamba <yashlamba2000@gmail.com>
2024-07-16 09:55:10 +0200

    📦 release: v13.0.0b0.dev7

--- invenio_app_rdm/administration/records/records.py
+++ invenio_app_rdm/administration/records/records.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023 CERN.
+# Copyright (C) 2023-2024 CERN.
 # Copyright (C) 2023 Graz University of Technology.
 #
 # invenio-administration is free software; you can redistribute it and/or
