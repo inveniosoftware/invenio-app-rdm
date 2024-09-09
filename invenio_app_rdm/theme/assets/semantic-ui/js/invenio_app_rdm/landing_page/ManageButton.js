6b2ba0072c03740c9301ea818ad69d5c4be93343
yashlamba <yashlamba2000@gmail.com>
2024-09-09 12:06:30 +0200

    landing_page: add block user option

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ManageButton.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ManageButton.js
@@ -5,9 +5,11 @@
 // under the terms of the MIT License; see LICENSE file for more details.
 
 import React from "react";
-import { Dropdown } from "semantic-ui-react";
+import { Dropdown, Modal, Button, Message } from "semantic-ui-react";
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 import PropTypes from "prop-types";
+import { http } from "react-invenio-forms";
+import { APIRoutes } from "../administration/users/api/routes";
 
 export const ManageButton = ({ recid, recordOwnerID }) => {
   return (
@@ -35,6 +37,8 @@ export const ManageButton = ({ recid, recordOwnerID }) => {
           key="manage_user"
           text={i18next.t("Manage user")}
         />
+        <Dropdown.Divider />
+        {recordOwnerID && <BlockUserItem recordOwnerID={recordOwnerID} />}
       </Dropdown.Menu>
     </Dropdown>
   );
@@ -42,5 +46,62 @@ export const ManageButton = ({ recid, recordOwnerID }) => {
 
 ManageButton.propTypes = {
   recid: PropTypes.string.isRequired,
+  recordOwnerID: PropTypes.string.isRequired,
+};
+
+const BlockUserItem = ({ recordOwnerID }) => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+  const blockUser = () => {
+    http.post(APIRoutes.block({ id: recordOwnerID }));
+    handleClose();
+  };
+
+  return (
+    <>
+      <Dropdown.Item
+        as="a"
+        className="error"
+        onClick={handleOpen}
+        key="block_user"
+        text={i18next.t("Block user")}
+      />
+      <Modal
+        open={modalOpen}
+        closeIcon
+        onClose={handleClose}
+        role="dialog"
+        closeOnDimmerClick={false}
+      >
+        <Modal.Header as="h2">{i18next.t("Block User")}</Modal.Header>
+        <Modal.Description>
+          <Message
+            warning
+            icon="warning sign"
+            content={i18next.t(
+              "Blocking the user will delete all existing records of the user."
+            )}
+          />
+        </Modal.Description>
+        <Modal.Actions>
+          <Button onClick={() => handleClose()} floated="left">
+            Cancel
+          </Button>
+          <Button
+            size="small"
+            labelPosition="left"
+            icon="warning"
+            color="red"
+            content={i18next.t("Block")}
+            onClick={blockUser}
+          />
+        </Modal.Actions>
+      </Modal>
+    </>
+  );
+};
+
+BlockUserItem.propTypes = {
   recordOwnerID: PropTypes.string.isRequired,
 };
