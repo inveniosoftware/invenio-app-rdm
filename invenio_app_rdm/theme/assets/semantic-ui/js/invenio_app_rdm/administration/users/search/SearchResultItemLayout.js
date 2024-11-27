b925dfaa64d0c260b74e692eeee85b4c7e7a6ae1
Sam Arbid <samaa2@kth.se>
2024-11-27 12:48:04 +0100

    fix: revert change after rebase

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/administration/users/search/SearchResultItemLayout.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/administration/users/search/SearchResultItemLayout.js
@@ -1,7 +1,6 @@
 /*
  * This file is part of Invenio.
  * Copyright (C) 2022-2024 CERN.
- * Copyright (C) 2024 KTH Royal Institute of Technology.
  *
  * Invenio is free software; you can redistribute it and/or modify it
  * under the terms of the MIT License; see LICENSE file for more details.
