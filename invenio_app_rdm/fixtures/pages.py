24fe2a77241caa5690c395d3f7db42b335d783c8
gressho <gressho@uni-muenster.de>
2025-01-22 16:15:55 +0100

    add copyright.

--- invenio_app_rdm/fixtures/pages.py
+++ invenio_app_rdm/fixtures/pages.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2022-2024 CERN.
+# Copyright (C)      2025 University of Münster.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
