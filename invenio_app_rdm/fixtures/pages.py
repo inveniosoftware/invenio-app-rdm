14e6fa568394762590bf09681ef10a346d0ae110
gressho <gressho@uni-muenster.de>
2025-01-22 16:03:05 +0100

    change: add internationalized page creation
    * when creation is forced.

--- invenio_app_rdm/fixtures/pages.py
+++ invenio_app_rdm/fixtures/pages.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2022-2024 CERN.
+# Copyright (C) 2025 University of Münster.
 #
 # Invenio App RDM is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -12,6 +13,7 @@ from pathlib import Path
 from flask import current_app
 from invenio_access.permissions import system_identity
 from invenio_db import db
+from invenio_i18n.proxies import current_i18n
 from invenio_pages.proxies import current_pages_service
 from invenio_pages.records.errors import PageNotFoundError
 from invenio_rdm_records.fixtures.fixture import FixtureMixin
@@ -25,6 +27,7 @@ class StaticPages(FixtureMixin):
         super().__init__(search_paths, filename)
         self._force = force
         self._pages_path = pages_path
+        self._supported_languages = current_i18n.get_languages()
 
     def load(self):
         """Load the static pages."""
@@ -34,30 +37,37 @@ class StaticPages(FixtureMixin):
         super().load()
         db.session.commit()
 
-    def page_data(self, page):
+    def page_data(self, page, lang):
         """Returns content of template."""
         for path in self._pages_path:
-            filepath = path / page
+            filepath = Path(str(path / page) + "." + lang)
 
             if not filepath.exists():
-                continue
+                filepath = path / page
+
+                if not filepath.exists():
+                    continue
 
             return Path(filepath).read_bytes().decode("utf8")
 
     def create(self, entry):
         """Load a single page."""
         url = entry["url"]
-        try:
-            current_pages_service.read_by_url(system_identity, url)
-        except PageNotFoundError:
-            data = {
-                "url": url,
-                "title": entry.get("title", ""),
-                # content is optional as it can be added later from the administration panel
-                "content": (
-                    self.page_data(entry["template"]) if entry.get("template") else ""
-                ),
-                "description": entry.get("description", ""),
-                "template_name": current_app.config["PAGES_DEFAULT_TEMPLATE"],
-            }
-            current_pages_service.create(system_identity, data)
+        for lang in self._supported_languages:
+            try:
+                current_pages_service.read_by_url(system_identity, url, lang[0])
+            except PageNotFoundError:
+                data = {
+                    "url": url,
+                    "title": entry.get("title", ""),
+                    # content is optional as it can be added later from the administration panel
+                    "content": (
+                        self.page_data(entry["template"], lang[0])
+                        if entry.get("template")
+                        else ""
+                    ),
+                    "lang": lang[0],
+                    "description": entry.get("description", ""),
+                    "template_name": current_app.config["PAGES_DEFAULT_TEMPLATE"],
+                }
+                current_pages_service.create(system_identity, data)
