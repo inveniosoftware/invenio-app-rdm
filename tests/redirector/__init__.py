acfa6a11b1ee45dfa2bb4d0e76b57311079b1c90
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-02-14 09:29:43 +0100

    tests: add __init__.py in all directories
    * This is necessary for pytest v8.x to be able to detect all unique
      tests.

---
+++ tests/redirector/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Graz University of Technology.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Tests."""
