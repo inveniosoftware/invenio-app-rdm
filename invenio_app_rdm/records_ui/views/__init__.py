3a5f0d6f3e77ff01cc7ae6f74aedbe1683edb49c
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2025-04-28 14:31:16 +0200

    records-ui: add error handler for `NoResultFound` exceptions

--- invenio_app_rdm/records_ui/views/__init__.py
+++ invenio_app_rdm/records_ui/views/__init__.py
@@ -22,6 +22,7 @@ from invenio_records_resources.services.errors import (
     PermissionDeniedError,
     RecordPermissionDeniedError,
 )
+from sqlalchemy.orm.exc import NoResultFound
 
 from invenio_app_rdm.views import create_url_rule
 
@@ -161,6 +162,7 @@ def create_blueprint(app):
     blueprint.register_error_handler(PIDUnregistered, not_found_error)
     blueprint.register_error_handler(KeyError, not_found_error)
     blueprint.register_error_handler(FileKeyNotFoundError, not_found_error)
+    blueprint.register_error_handler(NoResultFound, not_found_error)
     blueprint.register_error_handler(DraftNotCreatedError, draft_not_found_error)
     blueprint.register_error_handler(
         PermissionDeniedError, record_permission_denied_error
