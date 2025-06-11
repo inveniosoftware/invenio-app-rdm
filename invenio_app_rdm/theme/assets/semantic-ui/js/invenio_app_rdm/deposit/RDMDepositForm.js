769f055b6ae25417218b5c2b7b427a6daeac8aa7
Miroslav Bauer <mirekys@gmail.com>
2025-06-11 12:48:30 +0200

    deposit-ui: uppy uploader ui feature
    * addresses the UI part of RFC 0072
    
    * adds feature flag `APP_RDM_DEPOSIT_NG_FILES_UI` to enable the Uppy uploader UI
    
    * https://github.com/inveniosoftware/rfcs/pull/91

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
@@ -20,6 +20,7 @@ import {
   DeleteButton,
   DepositStatusBox,
   FileUploader,
+  UppyUploader,
   FormFeedback,
   IdentifiersField,
   PIDField,
@@ -114,6 +115,7 @@ export class RDMDepositForm extends Component {
       allowRecordRestriction,
       groupsEnabled,
       allowEmptyFiles,
+      useUppy,
     } = this.props;
 
     // Adding section id to custom fields UI, to be used for accordions
@@ -121,6 +123,7 @@ export class RDMDepositForm extends Component {
       ...section,
       id: section.section.toLowerCase().replace(/\s+/g, "-") + "-section",
     }));
+    const UploaderField = useUppy ? UppyUploader : FileUploader;
 
     return (
       <Overridable
@@ -196,13 +199,14 @@ export class RDMDepositForm extends Component {
                       filesLocked={filesLocked}
                       allowEmptyFiles={allowEmptyFiles}
                     >
-                      <FileUploader
+                      <UploaderField
                         isDraftRecord={!record.is_published}
                         quota={this.config.quota}
                         decimalSizeDisplay={this.config.decimal_size_display}
                         showMetadataOnlyToggle={permissions?.can_manage_files}
                         allowEmptyFiles={allowEmptyFiles}
                         filesLocked={filesLocked}
+                        fileUploadConcurrency={config.fileUploadConcurrency}
                       />
                     </Overridable>
                   </AccordionField>
@@ -782,6 +786,7 @@ RDMDepositForm.propTypes = {
   permissions: PropTypes.object,
   filesLocked: PropTypes.bool,
   allowEmptyFiles: PropTypes.bool,
+  useUppy: PropTypes.bool,
 };
 
 RDMDepositForm.defaultProps = {
