c23ea663770242b18e8735b1566c640ac06c9bd7
Sam Arbid <samaa2@kth.se>
2025-06-19 14:38:51 +0200

    i18n: refactor compile catalog
    * Exclude node_modules from i18next-scanner
    * Update extraction and compilation workflow for Transifex
    * Generate .pot as the source (English default)
    * Based on inveniosoftware/invenio-requests#408
    * Improved logging, error handling, and readability with async/await
    * Clarified translation pull instructions in docs

---
+++ invenio_app_rdm/theme/assets/semantic-ui/translations/invenio_app_rdm/messages/_generatedTranslations.js
@@ -0,0 +1,7 @@
+// This is an auto generated file to import and export all available translations.
+// Changes should not be checked into version control.
+import TRANSLATE_EN from "./en/translations.json";
+
+export const translations = {
+  en: { translation: TRANSLATE_EN },
+};
