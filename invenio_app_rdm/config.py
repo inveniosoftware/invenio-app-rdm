4cb61ab8cdde2436d45596e8578c4b526c688a73
Alex Ioannidis <a.ioannidis@cern.ch>
2025-06-20 10:45:59 +0200

    config: add Celery beat schedule for updating collections size

--- invenio_app_rdm/config.py
+++ invenio_app_rdm/config.py
@@ -491,6 +491,10 @@ CELERY_BEAT_SCHEDULE = {
         "task": "invenio_sitemap.tasks.update_sitemap_cache",
         "schedule": crontab(minute=0, hour=2),
     },
+    "update-collections-size": {
+        "task": "invenio_collections.tasks.update_collections_size",
+        "schedule": timedelta(hours=1),
+    },
 }
 """Scheduled tasks configuration (aka cronjobs)."""
 
