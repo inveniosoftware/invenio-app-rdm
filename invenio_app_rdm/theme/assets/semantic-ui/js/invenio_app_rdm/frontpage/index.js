b06a5ab3f5859dabb950edb8a7ba693729f6346d
Martin Fenner <martin@front-matter.io>
2025-07-20 12:56:26 +0200

    fix overridenComponents in frontpage

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/frontpage/index.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/frontpage/index.js
@@ -9,14 +9,13 @@ import ReactDOM from "react-dom";
 import { RecordsListOverridable } from "./RecordsList";
 import { OverridableContext, overrideStore } from "react-overridable";
 
-const overriddenComponents = overrideStore.getAll();
 const recordsListContainer = document.getElementById("records-list");
 const title = recordsListContainer.dataset.title;
 const fetchUrl = recordsListContainer.dataset.fetchUrl;
 const appName = "InvenioAppRDM.RecordsList";
 
 ReactDOM.render(
-  <OverridableContext.Provider value={overriddenComponents}>
+  <OverridableContext.Provider value={overrideStore.components}>
     <RecordsListOverridable title={title} fetchUrl={fetchUrl} appName={appName} />
   </OverridableContext.Provider>,
   recordsListContainer
