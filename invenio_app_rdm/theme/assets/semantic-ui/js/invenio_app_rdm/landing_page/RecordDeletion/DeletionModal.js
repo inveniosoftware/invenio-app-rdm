2da702e78698d96514b841f966c6726fa77656a7
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2025-07-25 17:27:03 +0200

    UI: add overridables to modal

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/RecordDeletion/DeletionModal.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/RecordDeletion/DeletionModal.js
@@ -8,6 +8,7 @@ import { i18next } from "@translations/invenio_app_rdm/i18next";
 import { Formik } from "formik";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import Overridable from "react-overridable";
 import {
   ErrorMessage,
   http,
@@ -94,135 +95,164 @@ export class DeletionModal extends Component {
     const { loading, error, checkboxes } = this.state;
 
     return (
-      <Modal
+      <Overridable
+        id="InvenioAppRDM.RecordDeletionModal.Layout"
+        handleClose={handleClose}
+        handleSubmit={this.handleSubmit}
+        deletionRedirectionConfig={deletionRedirectionConfig}
         open={open}
-        closeIcon
-        onClose={handleClose}
-        role="dialog"
-        aria-modal="true"
-        tab-index="-1"
-        size="tiny"
-        closeOnDimmerClick={false}
       >
-        <ModalHeader>Delete record</ModalHeader>
-        <ModalContent>
-          <Formik
-            onSubmit={this.handleSubmit}
-            validationSchema={this.deletionRequestSchema}
-            initialValues={{ reason: "", comment: "" }}
-            validateOnChange={false}
-            validateOnBlur={false}
-          >
-            {({ values, handleSubmit, errors }) => {
-              return (
-                <Form>
-                  <Message negative>
-                    <p>
-                      Deleting this record will delete <b>2</b> files.
-                    </p>
-                    <p>
-                      <b>Zenodo DOIs cannot be reused</b> and the DOI will resolve to a{" "}
-                      <a href="/">tombstone page</a>
-                    </p>
-                  </Message>
-                  <Table basic="very" unstackable>
-                    <TableHeader>
-                      <TableRow>
-                        <TableHeaderCell />
-                        <TableHeaderCell>Yes</TableHeaderCell>
-                        <TableHeaderCell>No</TableHeaderCell>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      {deletionRedirectionConfig.map((row, index) => (
-                        <DeletionRadioGroup
-                          index={index}
-                          row={row}
-                          key={row.name}
-                          state={checkboxes}
-                          onStateChange={this.handleRadioUpdate}
-                        />
-                      ))}
-                    </TableBody>
-                  </Table>
-                  <SelectField
-                    fluid
-                    fieldPath="reason"
-                    label={i18next.t("I want to delete this record because")}
-                    options={[
-                      {
-                        key: "duplicate",
-                        text: "Duplicate of another record",
-                        value: "duplicate",
-                      },
-                      {
-                        key: "retracted",
-                        text: "Retraction/Withdrawal of a record",
-                        value: "retracted",
-                      },
-                      {
-                        key: "test-record",
-                        text: "Test upload of a record",
-                        value: "test-record",
-                      },
-                      {
-                        key: "copyright",
-                        text: "Copyright infringement",
-                        value: "copyright",
-                      },
-                      {
-                        key: "personal-data",
-                        text: "Personal data issue",
-                        value: "personal-data",
-                      },
-                    ]}
-                    required
-                    disabled={checkboxes.some((x) => x || x === undefined)}
-                  />
-
-                  <TextAreaField
-                    fieldPath="comment"
-                    label={i18next.t("Comment")}
-                    required
-                    placeholder="Tell us more"
-                    disabled={checkboxes.some((x) => x || x === undefined)}
-                  />
-                  {error && (
-                    <ErrorMessage
-                      header={i18next.t("Unable to request deletion.")}
-                      content={i18next.t(error)}
-                      icon="exclamation"
-                      className="text-align-left"
-                      negative
+        <Modal
+          open={open}
+          closeIcon
+          onClose={handleClose}
+          role="dialog"
+          aria-modal="true"
+          tab-index="-1"
+          size="tiny"
+          closeOnDimmerClick={false}
+        >
+          <ModalHeader>Delete record</ModalHeader>
+          <ModalContent>
+            <Overridable id="InvenioAppRDM.RecordDeletionModal.Message">
+              <Message negative>
+                <p>
+                  Deleting this record will delete <b>2</b> files.
+                </p>
+                <p>
+                  <b>Zenodo DOIs cannot be reused</b> and the DOI will resolve to a{" "}
+                  <a href="/">tombstone page</a>
+                </p>
+              </Message>
+            </Overridable>
+            <Overridable
+              id="InvenioAppRDM.RecordDeletionModal.Table"
+              deletionRedirectionConfig={deletionRedirectionConfig}
+              handleRadioUpdate={this.handleRadioUpdate}
+              checkboxes={checkboxes}
+            >
+              <Table basic="very" unstackable>
+                <TableHeader>
+                  <TableRow>
+                    <TableHeaderCell />
+                    <TableHeaderCell>Yes</TableHeaderCell>
+                    <TableHeaderCell>No</TableHeaderCell>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {deletionRedirectionConfig.map((row, index) => (
+                    <DeletionRadioGroup
+                      index={index}
+                      row={row}
+                      key={row.name}
+                      state={checkboxes}
+                      onStateChange={this.handleRadioUpdate}
                     />
-                  )}
-                  <Button
-                    onClick={handleClose}
-                    content={i18next.t("Close")}
-                    className="left"
-                  />
-                  <Button
-                    content={i18next.t("Delete record immediately")}
-                    className="negative right floated"
-                    icon="trash alternate outline"
-                    labelPosition="left"
-                    onClick={(event) => handleSubmit(event)}
-                    loading={loading}
-                    positive
-                    disabled={checkboxes.some((x) => x || x === undefined) || loading}
-                  />
-                </Form>
-              );
-            }}
-          </Formik>
-        </ModalContent>
-        <ModalActions>
-          <p size="tiny">
-            Zenodo has a <a href="/">30 day grace period</a>. 28 days remain to delete
-            this record immediately
-          </p>
-        </ModalActions>
-      </Modal>
+                  ))}
+                </TableBody>
+              </Table>
+            </Overridable>
+            <Overridable
+              id="InvenioAppRDM.RecordDeletionModal.Formik"
+              handleSubmit={this.handleSubmit}
+              deletionRequestSchema={this.deletionRequestSchema}
+            >
+              <Formik
+                onSubmit={this.handleSubmit}
+                validationSchema={this.deletionRequestSchema}
+                initialValues={{ reason: "", comment: "" }}
+                validateOnChange={false}
+                validateOnBlur={false}
+              >
+                {({ handleSubmit }) => {
+                  return (
+                    <Form>
+                      <SelectField
+                        fluid
+                        fieldPath="reason"
+                        name="reason"
+                        label={i18next.t("I want to delete this record because")}
+                        options={[
+                          {
+                            key: "duplicate",
+                            text: "Duplicate of another record",
+                            value: "duplicate",
+                          },
+                          {
+                            key: "retracted",
+                            text: "Retraction/Withdrawal of a record",
+                            value: "retracted",
+                          },
+                          {
+                            key: "test-record",
+                            text: "Test upload of a record",
+                            value: "test-record",
+                          },
+                          {
+                            key: "copyright",
+                            text: "Copyright infringement",
+                            value: "copyright",
+                          },
+                          {
+                            key: "personal-data",
+                            text: "Personal data issue",
+                            value: "personal-data",
+                          },
+                        ]}
+                        required
+                        disabled={checkboxes.some((x) => x || x === undefined)}
+                      />
+
+                      <TextAreaField
+                        fieldPath="comment"
+                        name="comment"
+                        label={i18next.t("Comment")}
+                        required
+                        placeholder="Tell us more"
+                        disabled={checkboxes.some((x) => x || x === undefined)}
+                      />
+                      {error && (
+                        <ErrorMessage
+                          header={i18next.t("Unable to request deletion.")}
+                          content={i18next.t(error)}
+                          icon="exclamation"
+                          className="text-align-left"
+                          negative
+                        />
+                      )}
+                      <Button
+                        onClick={handleClose}
+                        content={i18next.t("Close")}
+                        className="left"
+                      />
+                      <Button
+                        content={i18next.t("Delete record immediately")}
+                        className="negative right floated"
+                        icon="trash alternate outline"
+                        labelPosition="left"
+                        onClick={(event) => handleSubmit(event)}
+                        loading={loading}
+                        positive
+                        disabled={
+                          checkboxes.some((x) => x || x === undefined) || loading
+                        }
+                      />
+                    </Form>
+                  );
+                }}
+              </Formik>
+            </Overridable>
+          </ModalContent>
+          <Overridable id="InvenioAppRDM.RecordDeletionModal.ModalActions">
+            <ModalActions>
+              <p size="tiny">
+                Zenodo has a <a href="/">30 day grace period</a>. 28 days remain to
+                delete this record immediately
+              </p>
+            </ModalActions>
+          </Overridable>
+        </Modal>
+      </Overridable>
     );
   }
 }
