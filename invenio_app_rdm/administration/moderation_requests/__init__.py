49d9a92c1f701ee01d4b78dbb21fbdc48d8bb3cc
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2025-08-08 17:49:03 +0200

    admin: create moderation->requests panel

---
+++ invenio_app_rdm/administration/moderation_requests/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 CERN.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""User moderation administration module."""
+
+from .moderation_requests import ModerationRequestListView
+
+__all__ = ("ModerationRequestListView")
---
+++ invenio_app_rdm/administration/moderation_requests/moderation_requests.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 CERN.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""Invenio administration view module for user moderation."""
+
+from functools import partial
+
+from flask import current_app
+from invenio_administration.views.base import (
+    AdminResourceDetailView,
+    AdminResourceListView,
+)
+from invenio_i18n import lazy_gettext as _
+from invenio_rdm_records.requests import RecordDeletion
+from invenio_search_ui.searchconfig import search_app_config
+
+
+class ModerationRequestListView(AdminResourceListView):
+    """User moderation admin search view."""
+
+    api_endpoint = "/requests"
+    extension_name = "invenio-requests"
+    name = "requests"
+    resource_config = "requests_resource"
+    request_headers = {"Accept": "application/vnd.inveniordm.v1+json"}
+    title = _("Requests")
+    menu_label = _("Requests")
+    category = _("Moderation")
+    pid_path = "id"
+    icon = "mail"
+    template = "invenio_app_rdm/administration/requests.html"
+    order = 1
+
+    display_search = True
+    display_delete = False
+    display_create = False
+    display_edit = False
+    display_read = False
+
+    search_config_name = "REQUESTS_MODERATION_REQUEST_SEARCH"
+    search_facets_config_name = "REQUESTS_MODERATION_REQUEST_FACETS"
+    search_sort_config_name = "REQUESTS_MODERATION_REQUEST_SORT_OPTIONS"
+
+    item_field_list = {
+        "type": {"text": _("Type"), "order": 1, "width": 3},
+        "created": {"text": _("Created"), "order": 2, "width": 3},
+        "last-reply": {"text": _("Last reply"), "order": 3, "width": 3},
+    }
+
+    @property
+    def request_type(self):
+        """Request type property."""
+        request_type = self.resource.service.request_type_registry.lookup(
+            "record-deletion"
+        )
+        return request_type
+
+    @classmethod
+    def get_service_schema(cls):
+        """Get marshmallow schema of the assigned service."""
+        request_type = cls.resource.service.request_type_registry.lookup(
+            "record-deletion"
+        )
+        # handle dynamic schema class declaration for requests
+        schema_cls = request_type.marshmallow_schema()
+        schema = schema_cls()
+        return schema
+
+    @staticmethod
+    def disabled():
+        """Disable the view on demand."""
+        return False
+
+    def init_search_config(self):
+        """Build search view config."""
+        return partial(
+            search_app_config,
+            config_name=self.get_search_app_name(),
+            available_facets=current_app.config.get(self.search_facets_config_name),
+            sort_options=current_app.config[self.search_sort_config_name],
+            endpoint=self.get_api_endpoint(),
+            headers=self.get_search_request_headers(),
+            initial_filters=[["is_open", "true"]],
+            hidden_params=[
+                ["expand", "1"],
+                ["type", RecordDeletion.type_id],
+            ],
+            pagination_options=(20, 50),
+            default_size=20,
+        )
