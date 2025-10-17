b4ad1b92306b6d5e82766551a73177b99cd32df3
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2025-10-17 13:32:58 +0200

    chore: relax proptypes

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
@@ -803,8 +803,8 @@ RDMDepositForm.propTypes = {
   config: PropTypes.object.isRequired,
   recordRestrictionGracePeriod: PropTypes.number.isRequired,
   allowRecordRestriction: PropTypes.bool.isRequired,
-  recordDeletion: PropTypes.object.isRequired,
-  fileModification: PropTypes.object.isRequired,
+  recordDeletion: PropTypes.object,
+  fileModification: PropTypes.object,
   record: PropTypes.object.isRequired,
   preselectedCommunity: PropTypes.object,
   files: PropTypes.object,
@@ -821,4 +821,6 @@ RDMDepositForm.defaultProps = {
   filesLocked: false,
   allowEmptyFiles: true,
   useUppy: false,
+  recordDeletion: {},
+  fileModification: {},
 };
