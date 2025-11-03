3266655c9014afaccc86bb472ad988def11b137f
Sam Arbid <samaa2@kth.se>
2025-11-03 18:27:04 +0100

    feat(admin): add roles management views
    * Add role list, detail, create, and edit views
    * Surface roles on the user detail page.
    * Restrict user and role to authorized admins only.

---
+++ invenio_app_rdm/administration/users/permissions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 CERN.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Helpers for administration moderation access checks."""
+
+from flask import g
+from flask_principal import RoleNeed
+from invenio_access.permissions import superuser_access
+
+
+def can_access_user_administration(identity=None):
+    """Return True if the identity may access user/role admin views."""
+    identity = identity or getattr(g, "identity", None)
+    if not identity:
+        return False
+
+    provides = identity.provides
+    admin_need = RoleNeed("administration")
+    moderator_need = RoleNeed("administration-moderation")
+
+    return (superuser_access in provides) or (
+        admin_need in provides and moderator_need in provides
+    )
