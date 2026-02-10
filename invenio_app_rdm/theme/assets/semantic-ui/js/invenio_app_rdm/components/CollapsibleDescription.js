5333f40ff9f29a9ecaa118eb14fb01ace8ae478c
joaocasr <castrojoao839@gmail.com>
2026-02-10 16:33:18 +0000

    component: RecordsResultsListItem
    * NEW Adds a CollapsibleDescription component with a collapsible/expandable record description.
    
    * BETTER Improves UX by allowing users to view full descriptions inline without navigating to the record detail page.
    
    Signed-off-by: joaocasr <castrojoao839@gmail.com>

---
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/components/CollapsibleDescription.js
@@ -0,0 +1,53 @@
+/**
+ * CollapsibleDescription
+ *
+ * This component will expand and collapse the record description.
+ *
+ * Props:
+ * - descriptionStripped: string or node containing the record description
+ */
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Item, Icon } from "semantic-ui-react";
+import classNames from "classnames";
+
+const CollapsibleDescription = ({ descriptionStripped }) => {
+  const [open, setOpen] = useState(false);
+
+  if (!descriptionStripped) return null;
+
+  return (
+    <>
+      <Item.Description
+        className={classNames({
+          "truncate-lines-2": !open,
+        })}
+      >
+        {descriptionStripped}
+      </Item.Description>
+      
+      <div
+        onClick={() => setOpen((v) => !v)}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          cursor: "pointer",
+          marginTop: "0.25rem",
+        }}
+        aria-label={open ? "Collapse description" : "Expand description"}
+        role="button"
+      >
+        <Icon
+            name={open ? "angle up" : "angle down"}
+            size="large"
+            style={{ color: "#0377cd" }}
+          
+        />
+      </div>
+    </>
+  );
+};
+
+CollapsibleDescription.propTypes = {
+  descriptionStripped: PropTypes.node,
+};
