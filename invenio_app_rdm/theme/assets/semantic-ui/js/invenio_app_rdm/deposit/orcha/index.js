b87194f4c1853d935d0c78f0e03853d05b68f43d
Oliver Geneser <hello@geneser.xyz>
2026-06-08 12:48:30 +0200

    feat: orcha

---
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/orcha/index.js
@@ -0,0 +1,4 @@
+export { SuggestionsContext, useSuggestions } from "./Context";
+export { SuggestionsProvider } from "./Provider";
+export { FieldSuggestion } from "./FieldSuggestion";
+export { SuggestionsButton } from "./Button";
