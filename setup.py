8e1a3f16a1bbfbd52e037aced52aa3eb617e4cbc
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-29 15:18:23 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * bump requires-python due to dependencies requiring 3.9+
    * declare conflicting extras for uv
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2019-2022 CERN.
-# SPDX-FileCopyrightText: 2019-2022 Northwestern University.
-# SPDX-FileCopyrightText: 2021-2022 TU Wien.
-# SPDX-License-Identifier: MIT
-
-from setuptools import setup
-
-setup()
