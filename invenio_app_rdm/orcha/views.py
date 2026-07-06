e02445df18f9c2e19e8c8ec461a084c85a9ebbfa
Oliver Geneser <hello@geneser.xyz>
2026-07-06 14:22:41 +0200

    feat: add user id

--- invenio_app_rdm/orcha/views.py
+++ invenio_app_rdm/orcha/views.py
@@ -100,6 +100,7 @@ def get_workflow_stream_url(pid_value):
     file_key = data.get("fileKey")
     payload = {
         "workflow_type": "extract_metadata",
+        "user_id": str(g.identity.id),
         "params": {"url": _file_download_url(pid_value, orcha, key=file_key)},
     }
 
