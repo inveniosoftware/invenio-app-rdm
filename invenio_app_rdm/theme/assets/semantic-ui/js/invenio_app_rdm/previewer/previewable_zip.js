61812226f6d4c59cd85be54a67e49d385bcff26d
Martin Čorovčák <martino.coro@gmail.com>
2026-09-23 12:53:57 +0200

    fix(previewer): resolve race condition in ZIP file preview handling

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/previewer/previewable_zip.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/previewer/previewable_zip.js
@@ -5,21 +5,31 @@
 
 import $ from "jquery";
 
-const channel = new BroadcastChannel("invenio-previewer-zip");
+window.addEventListener("message", onMessage);
 
-$(".preview-link").on("click", function (e) {
-  e.preventDefault();
-  const fileKey = $(this).data("file-key");
-  const previewUrl = $(this).data("preview-url");
-  const containerFileKey = $(this).data("container-file-key");
-  const containerPreviewUrl = window.location.href;
-  channel.postMessage({
-    fileKey: fileKey,
-    previewUrl: previewUrl,
-    containerFileKey: containerFileKey,
-    containerPreviewUrl: containerPreviewUrl,
+function onMessage(msg) {
+  if (msg.origin !== window.location.origin || !msg.ports.length || msg.data?.type !== "invenio-previewer-zip") return;
+
+  const channelId = msg.data?.channelId;
+  if (!channelId) return;
+
+  $(".preview-link").on("click", function (e) {
+    e.preventDefault();
+    const fileKey = $(this).data("file-key");
+    const previewUrl = $(this).data("preview-url");
+    const containerFileKey = $(this).data("container-file-key");
+    const containerPreviewUrl = window.location.href;
+    
+    msg.ports[0].postMessage({
+      type: "invenio-previewer-zip",
+      channelId: channelId,
+      fileKey: fileKey,
+      previewUrl: previewUrl,
+      containerFileKey: containerFileKey,
+      containerPreviewUrl: containerPreviewUrl,
+    });
+    console.log(
+      `[${containerFileKey} at ${containerPreviewUrl}] Requested preview for fileKey: ${fileKey} with URL: ${previewUrl}`
+    );
   });
-  console.log(
-    `[${containerFileKey} at ${containerPreviewUrl}] Requested preview for fileKey: ${fileKey} with URL: ${previewUrl}`
-  );
-});
+}
